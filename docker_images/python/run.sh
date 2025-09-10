7579157075dff2036c54589cc2580cc0f8c4239e
Titov Evgeniy <evgeniy.titov@eltex.loc>
2025-09-10 10:38:18 +0700

    change build docker python docker images

--- docker_images/python/run.sh
+++ docker_images/python/run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #
-docker build -t flask-ip-info .
-docker run --rm -p 5000:5000 flask-ip-info
-
+docker build -t python-hostname:1.0 .
+docker run --rm -p 5000:5000 python-hostname:1.0
+docker tag python-hostname:1.0 ghcr.io/titov32/python-hostname:1.0
+docker push ghcr.io/titov32/python-hostname:1.0
